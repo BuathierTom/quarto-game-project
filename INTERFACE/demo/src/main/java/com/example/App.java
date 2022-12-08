e2678348d3df74105939cac2f6b0b446b02ece5a
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2022-12-08 14:25:53 +0100

    Tests et familiarisation avec JAVAFX

--- INTERFACE/demo/src/main/java/com/example/App.java
+++ INTERFACE/demo/src/main/java/com/example/App.java
@@ -2,9 +2,12 @@ package com.example;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
 
 /**
  * JavaFX App
@@ -13,14 +16,35 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        VBox v1 = new VBox();
+        HBox h1 = new HBox();
+        
+        GridPane pane = new GridPane();
+
+                
+        Button play = new Button("PLAY");
+        Button regles = new Button("REGLES");
+        Button exit = new Button("EXIT");
+
+
+
+        h1.getChildren().add(play);    
+        h1.getChildren().add(regles);    
+        h1.getChildren().add(exit);  
+
+
+        v1.getChildren().addAll(h1);
+
+
+        
+        stage.setTitle("QUARTO");
+        Scene scene = new Scene(v1, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
+
+    
 
     public static void main(String[] args) {
         launch();
