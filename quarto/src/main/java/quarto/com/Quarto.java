9103cd988c98dbdba53e40327a561312dadd5ed7
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2022-12-15 15:55:02 +0100

    Installation de la grille avec les boutons

--- quarto/src/main/java/quarto/com/Quarto.java
+++ quarto/src/main/java/quarto/com/Quarto.java
@@ -1,15 +1,20 @@
 package quarto.com;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 
@@ -19,67 +24,40 @@ public class Quarto {
     public static Stage quartoWindow(Stage mainWindow){
         
         GridPane layout = new GridPane();
-        HBox a = new HBox();
-        HBox b = new HBox();
-        HBox c = new HBox();
-        HBox d = new HBox();
-
 
         /* Placement automatique mais non fonctionnel a reprendre plus tard */
-        // Button grille = new Button(" ");
-        // ArrayList<Button> grilleCompleted = new ArrayList<>() ;
-        // for(int i = 0 ; i < 4 ; i++)
-	    //     for(int j = 0; j < 4 ; j++){
-        //         grille = new Button("A" + i);
-        //         grilleCompleted.add(grille);
-        //         for (Button button : grilleCompleted) {
-        //             for(int z = 0 ; z < 4 ; z++){
-        //                 a.getChildren().addAll(button);
-        //                 layout.addRow(z , a);
-        //             }
-        //         }
-        // }
-        /* Placemment deguelasse mais fonctionnel */
+        Button box = new Button(" ");
+	    for(int j = 0; j < 4 ; j++){
+            box = new Button("A");  
+            layout.addRow(0 , box);
+            
+            GridPane.setMargin(box, new Insets(10, 10, 10, 10));
+        }
+        for(int j = 0; j < 4 ; j++){
+            box = new Button("B");  
+            layout.addRow(1 , box);
+            
+            GridPane.setMargin(box, new Insets(10, 10, 10, 10));
+        }
+        for(int j = 0; j < 4 ; j++){
+            box = new Button("C");  
+            layout.addRow(2 , box);
+
+            GridPane.setMargin(box, new Insets(10, 10, 10, 10));
+        }
+        for(int j = 0; j < 4 ; j++){
+            box = new Button("D");  
+            layout.addRow(3 , box);
+
+            GridPane.setMargin(box, new Insets(10, 10, 10, 10));
+        }
+
+        layout.setAlignment(Pos.CENTER);
+        GridPane.setHgrow(box, Priority.ALWAYS);
+
+        // 'https://www.delftstack.com/fr/howto/java/javafx-square-button/'
         
-        Button a1 = new Button("A1");
-        Button a2 = new Button("A2");
-        Button a3 = new Button("A3");
-        Button a4 = new Button("A4");
         
-        a.getChildren().addAll(a1, a2, a3, a4);
-
-        Button b1 = new Button("B1");
-        Button b2 = new Button("B2");
-        Button b3 = new Button("B3");
-        Button b4 = new Button("B4");
-
-        b.getChildren().addAll(b1, b2, b3, b4);
-
-        Button c1 = new Button("C1");
-        Button c2 = new Button("C2");
-        Button c3 = new Button("C3");
-        Button c4 = new Button("C4");   
-
-        c.getChildren().addAll(c1, c2, c3, c4);
-
-        Button d1 = new Button("D1");
-        Button d2 = new Button("D2");
-        Button d3 = new Button("D3");
-        Button d4 = new Button("D4");
-
-        d.getChildren().addAll(d1, d2, d3, d4);
-
-
-        //Ajout des boutons au GridPane        
-        layout.addRow(0 , a);
-        layout.addRow(1 , b);
-        layout.addRow(2 , c);
-        layout.addRow(3 , d);
-
-
-
-        
-                    
         Scene secondScene = new Scene(layout, 640, 480);
         Stage playWindows = new Stage();
         playWindows.setTitle("QUARTO");
