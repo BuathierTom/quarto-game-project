7463e06cc59735345f3f5d76b92fa278ace1b38d
BuathierTom <buathier_tom@orange.fr>
2023-03-25 16:01:29 +0100

    Background

--- quarto/src/main/java/quarto/com/WinWindow.java
+++ quarto/src/main/java/quarto/com/WinWindow.java
@@ -6,6 +6,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -21,6 +26,11 @@ public class WinWindow {
         BorderPane centralLayout = new BorderPane();
 
 
+
+        Image image = new Image("fond.jpg");
+        BackgroundSize backgroundSize = new BackgroundSize(1400, 700, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
         Scene sceneWindow = new Scene(centralLayout, 1400, 700);
         Stage stageWindow = new Stage();
         /* Icone du jeu : */        
